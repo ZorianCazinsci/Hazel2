ffc66c763d0b739dcedfcf75426edff7e53789b6
ZorianCazinscii <glavnoevovsem@gmail.com>
2025-11-03 10:59:07 -0800

    +логирование

--- src/Hazel/EntryPoint.h
+++ src/Hazel/EntryPoint.h
@@ -6,10 +6,15 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) 
 {
+    Hazel::Log::Init();
+    HZ_CORE_WARN("Initialized Log!");
+    int a = 5;
+    HZ_INFO("Hello! Var {}", a);
+    
     auto app = Hazel::CreateApplication();
     app->Run();
     delete app;
-
+     
 }   
 
 
